0ece0cac0db381c15c66d57397e7c67496a0a3d3
Easy Code <easycode002@gmail.com>
2024-12-03 09:50:40 +0700

    Added: ...

--- file/api/view.py
+++ file/api/view.py
@@ -123,7 +123,6 @@ class FileUploadView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-
 # View file headers
 class ViewHeaderView(APIView):
     permission_classes = [permissions.AllowAny]
